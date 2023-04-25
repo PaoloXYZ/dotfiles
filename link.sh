f2ed8525fc107e88db0bd1e7ecc283b17fab665f
PaoloXYZ <paolomaestri01@gmail.com>
2023-04-25 17:15:00 +1200

    first commit

---
+++ link.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "linking!"
+
+ln -sv $HOME/git/dotfiles/x/.xprofile $HOME
+ln -sv $HOME/git/dotfiles/alacritty $HOME/.config
+ln -sv $HOME/git/dotfiles/i3 $HOME/.config
+ln -sv $HOME/git/dotfiles/i3status $HOME/.config
+ln -sv $HOME/git/dotfiles/qBittorrent $HOME/.config
+ln -sv $HOME/git/dotfiles/zathura $HOME/.config
+ln -sv $HOME/git/dotfiles/zsh $HOME/.config
